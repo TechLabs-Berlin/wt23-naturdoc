36ef0add6e66be6dbcdbecce784970a95a7933c3
Rose <rose.jeantet@gmail.com>
2023-03-05 11:07:22 +0100

    adding possibility to limit the number of results for performance and usability reasons

--- reactApp/src/components/results/ResultList.js
+++ reactApp/src/components/results/ResultList.js
@@ -2,7 +2,7 @@ import ResultShow from "./ResultShow";
 import { Container, Paper } from "@mui/material";
 
 function ResultList({ remedies }) {
-  const renderedRemedies = remedies.map((remedy) => {
+  const renderedRemedies = remedies.slice(0, 10).map((remedy) => {
     return <ResultShow key={remedy.id} remedy={remedy} />;
   });
 
