9dabfc06b05be39e11268449a215197f180fe2ea
ChristinaLisa <christina.dechent@hotmail.de>
2023-03-12 21:39:02 +0100

    add /register, add /login, add /logout, add .gitignore (remove node_modules & views directory from git), add middleware to check login, add connect-flash to flash success & error messages, add ratings model

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express');
-
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
-const Medicals = require('./models/medicals')
-//const catchAsynch = require('../utilities/catchAsynch');
+const session = require('express-session');
+const Medicals = require('./models/medicals');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const flash = require('connect-flash');
+const User = require('./models/ratings');
+const catchAsynch = require('./utilities/catchAsynch');
+const { checkLogin } = require('./middleware');
 
 mongoose.connect('mongodb://localhost:27017/naturdoc');
+
+//mongoose.connect('mongodb+srv://naturdoc:WhYJmBoDdO3tZ89Z@naturdoc.aj9zhtw.mongodb.net/?retryWrites=true&w=majority');
+
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection.error:"))
@@ -17,13 +25,103 @@ db.once("open", () => {
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+const sessionConfig = {
+    secret: 'testing',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+        httpOnly: true
+    }
+}
+app.use(express.json());
+app.use(session(sessionConfig));
+app.use(flash());
+app.use((req, res, next) => {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// homepage
 app.get('/', (req, res) => {
     res.render('form')
 });
 
+//app.get('/test', async (req, res) => {
+//    const user = new User({ email: 'cristy@gmail.com', username: 'cristy' });
+//    const newUser = await User.register(user, 'cats');
+//    res.send(newUser);
+//})
+
+
+//query field -> DS? 
 app.get('/search', async (req, res) => {
-    const medical = await medicals.find({});
+    //
+    res.render('form', { medicals })
+});
+
+//medicals page 
+app.get('/medicals/:id', async (req, res) => {
+    const medical = await Medicals.findById(req.params.id);
     res.render('form')
+});
+
+app.put('/medicals/:id', async (req, res) => {
+    const { id } = req.params;
+    const rating = await Ratings.findByIdAndUpdate(id, { ...req.body.rating });
+    res.redirect(`/medicals/${Medicals._id}`);
+})
+
+//users endpoints: 
+
+app.get('/register', (req, res) => {
+    res.render('user')
+})
+
+app.post('/register', catchAsynch(async (req, res) => {
+    try {
+        console.log(req);
+        const { email, username, password } = req.body;
+        const user = new User({ email, username })
+        const registeredUser = await User.register(user, password);
+        req.login(registeredUser, err => {
+            if (err) return next(err);
+            req.flash('success', 'Successfully logged in');
+            res.redirect('/');
+        })
+
+    }
+    catch (e) {
+        req.flash('error', e.message);
+        res.redirect('register');
+    }
+}));
+
+app.get('/login', (req, res) => {
+    res.render('login')
+});
+
+app.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+    req.flash('success', 'welcome back');
+    res.redirect('form')
+});
+
+app.get('/logout', function (req, res, next) {
+    req.logout(function (err) {
+        if (err) {
+            return next(err);
+        }
+        req.flash('success', "Successfully logged out!");
+        res.redirect('/');
+    });
 });
 
 app.listen(7000, () => {
