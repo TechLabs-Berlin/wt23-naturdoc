ebfe6b92780ec87dcb10cb8032f0382f2201f38f
alj-b <aljoscha_b@icloud.com>
2023-03-16 00:15:15 +0100

    First pydantic code

--- ds/src/main.py
+++ ds/src/main.py
@@ -1,9 +1,14 @@
 from typing import Union
-
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None # becomes optional
+    price: float
+    tax: Union[float, None] = None
 
 @app.get("/")
 def read_root():
@@ -16,6 +21,10 @@ async def read_item(q: Union[str, None] = None):
         "taxonomicName": "Bla", 
         "userSymptoms": q
         }
+
+@app.post("/items/")
+async def create_item(item: Item): # declaring it as a required parameter
+    return item
 
 # run the server in bash with 
 # uvicorn main:app --reload
