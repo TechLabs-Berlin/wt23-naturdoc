b8054202e62da3ac10a1b334d96d86e9485c9522
Rose <rose.jeantet@gmail.com>
2023-03-25 19:27:12 +0100

    renderRating cases work-in-progress2

--- reactApp/src/components/ratings/AddRating.js
+++ reactApp/src/components/ratings/AddRating.js
@@ -1,15 +1,12 @@
 import { Typography } from "@mui/material";
 
-function AddRating(notRatedByUser, notRatingsYet) {
+function AddRating(ratings, notRatedByUser, notRatingsYet) {
   return (
     <>
+      {ratings.length} matching reviews
       {/* Case 1a: No reviews yet */}
       <Typography variant="remedyTitle"></Typography>
-
       {/* Case 1B: No reviews from user  yet */}
-      <Typography variant="remedyTitle"></Typography>
-
-      {/* Case 1C: Display review from user */}
       <Typography variant="remedyTitle"></Typography>
     </>
   );
