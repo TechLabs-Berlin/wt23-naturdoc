b43f3c5963f495bb63fbb29e4c51e21b4d12e36f
Rose <rose.jeantet@gmail.com>
2023-03-27 13:20:26 +0200

    work in progress rating system components allocation

--- reactApp/src/components/ratings/AddRating.js
+++ reactApp/src/components/ratings/AddRating.js
@@ -1,6 +1,14 @@
 import { Typography } from "@mui/material";
+import { Card, Box, Button } from "@mui/material/";
+import ChevronRight from "@mui/icons-material/ChevronRight";
 
-function AddRating(ratings, notRatedByUser, notRatingsYet) {
+function AddRating(
+  remedy,
+  ratings,
+  notRatedByUser,
+  notRatingsYet,
+  handleClickOpen
+) {
   return (
     <>
       {ratings.length} matching reviews
@@ -8,6 +16,24 @@ function AddRating(ratings, notRatedByUser, notRatingsYet) {
       <Typography variant="remedyTitle"></Typography>
       {/* Case 1B: No reviews from user  yet */}
       <Typography variant="remedyTitle"></Typography>
+      <Card variant="reviewCard">
+        <Typography variant="body1" sx={{ textAlign: "center" }} paragraph>
+          {remedy.ratingAverage
+            ? "You have not shared your opinion yet. What do you think of this remedy?"
+            : "Be the first to add a review."}
+        </Typography>
+        <Box sx={{ display: "flex", justifyContent: "center" }}>
+          <Button
+            align="center"
+            size="large"
+            variant="outlined"
+            onClick={handleClickOpen}
+            endIcon={<ChevronRight />}
+          >
+            Write a review
+          </Button>
+        </Box>
+      </Card>
     </>
   );
 }
--- reactApp/src/pages/RemedyDetails.js
+++ reactApp/src/pages/RemedyDetails.js
@@ -11,15 +11,10 @@ import RemedyAccordion from "components/remedy/RemedyAccordion";
 import RatingForm from "components/ratings/RatingForm";
 import RatingList from "components/ratings/RatingList";
 import LayoutHOC from "components/layouts/LayoutHOC";
+import AddRating from "components/ratings/AddRating";
+
 // material-ui
-import {
-  CardMedia,
-  Box,
-  Container,
-  IconButton,
-  Paper,
-  Button,
-} from "@mui/material";
+import { CardMedia, Box, Container, IconButton } from "@mui/material";
 import Typography from "@mui/material/Typography";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import RemedyRating from "components/remedy/RemedyRating";
@@ -58,32 +53,41 @@ function RemedyDetails() {
     setOpen(false);
   };
 
-  /* function joinContent(contentMatched) {
-     return <>{contentMatched ? contentMatched.join(", ") : " "}</>;
-  } */
+  function formatString(contentMatched) {
+    return (
+      <>
+        {" "}
+        {contentMatched ? contentMatched.toString().replace(/,/g, ", ") : null}
+      </>
+    );
+  }
 
   return (
     <>
-      <Container sx={{ pl: 0, pr: 0 }} component="section" maxWidth="sm">
-        <Paper>
-          <Box>
-            <IconButton variant="outlined" onClick={() => navigate(-1)}>
-              <ChevronLeftIcon fontSize="large" />
-            </IconButton>
-          </Box>
-          <Box sx={{ position: "relative", height: "194px" }}>
-            <CardMedia
-              component="img"
-              height="194"
-              image="https://placehold.co/600x194"
-              alt="Remedy image"
-              sx={{ position: "absolute" }}
-            />
-            {/* <Box sx={{ position: "absolute", top: "156px", left: "16px" }}>
+      <Container sx={{ pl: 0, pr: 0, pb: 8 }} component="section" maxWidth="sm">
+        <Box>
+          <IconButton variant="outlined" onClick={() => navigate(-1)}>
+            <ChevronLeftIcon fontSize="large" />
+          </IconButton>
+        </Box>
+
+        {/* Remedy Image */}
+        <Box component="div" sx={{ position: "relative", height: "194px" }}>
+          <CardMedia
+            component="img"
+            height="194"
+            image="https://placehold.co/600x194"
+            alt="Remedy image"
+            sx={{ position: "absolute" }}
+          />
+          {/* <Box sx={{ position: "absolute", top: "156px", left: "16px" }}>
                 <RemedyIcon remedy={remedy} smallIcon={false} />
               </Box> */}
-          </Box>
-          <Box sx={{ p: 2, pt: -2 }}>
+        </Box>
+
+        <Box component="div" sx={{ p: 2, pt: -2 }}>
+          {/* Remedy header */}
+          <Box component="div">
             <Typography variant="remedyTitle" component="h1">
               {remedy.remedyName}
             </Typography>
@@ -99,29 +103,26 @@ function RemedyDetails() {
             ) : (
               <RemedyRating remedy={remedy} />
             )}
-            {!remedy.symptomsMatched ? (
-              " "
-            ) : (
-              <>
-                <Typography variant="body" sx={{ fontSize: "0.81rem" }}>
-                  Recommended use for:
-                </Typography>
-                <Typography variant="body2" sx={{ fontWeight: "600" }}>
-                  {remedy.symptomsMatched.join(", ")}
-                </Typography>
-              </>
-            )}
+
+            <Typography variant="body" sx={{ fontSize: "0.81rem" }}>
+              {remedy.medicinalUses ? "Recommended use for:" : " "}
+            </Typography>
+            <Typography variant="body2" sx={{ fontWeight: "600" }}>
+              {formatString(remedy.medicinalUses)}
+            </Typography>
           </Box>
-          <Box sx={{ mx: 2 }}>
+
+          {/* Remedy Accordion */}
+          <Box component="div" sx={{ py: 2 }}>
             <RemedyAccordion
               remedy={remedy}
               accordionSummary={"Other common names"}
-              accordionDetails={remedy.commonNames}
+              accordionDetails={formatString(remedy.commonNames)}
             />
             <RemedyAccordion
               remedy={remedy}
               accordionSummary={"Activities"}
-              accordionDetails={remedy.medicinalUses}
+              accordionDetails={formatString(remedy.medicinalUses)}
             />
             <RemedyAccordion
               remedy={remedy}
@@ -158,16 +159,14 @@ function RemedyDetails() {
               accordionSummary={"Posology / Dosage"}
               accordionDetails={remedy.posology}
             />
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={handleClickOpen}
-            >
-              Write a Review
-            </Button>
-            <RatingList ratings={ratings} />
-          </Box>{" "}
-        </Paper>{" "}
+          </Box>
+
+          {/* Review Area */}
+          <Box component="div">
+            <AddRating remedy={remedy} handleClickOpen />
+          </Box>
+          <RatingList ratings={ratings} />
+        </Box>
       </Container>
 
       <RatingForm
