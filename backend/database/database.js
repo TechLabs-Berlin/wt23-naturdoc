969f782fad808cc6733cd6c6d2a0c75cdfffca3e
ChristinaLisa <christina.dechent@hotmail.de>
2023-03-29 09:17:31 +0200

    comment out url

--- backend/database/database.js
+++ backend/database/database.js
@@ -5,13 +5,13 @@ dotenv.config();
 const { MONGO_URI } = process.env;
 
 
-//const mongourl = 'mongodb+srv://naturdoc:WhYJmBoDdO3tZ89Z@naturdoc.aj9zhtw.mongodb.net/naturdoc?retryWrites=true&w=majority';
+const mongourl = 'mongodb+srv://naturdoc:WhYJmBoDdO3tZ89Z@naturdoc.aj9zhtw.mongodb.net/naturdoc?retryWrites=true&w=majority';
 //const mongourl = MONGO_URI;
 //const mongourl = 'mongodb://localhost:27017/naturdoc';
 
 // db connection
 function connect() {
-  return mongoose.connect(MONGO_URI);
+  return mongoose.connect(mongourl);
 }
 
 function close() {
