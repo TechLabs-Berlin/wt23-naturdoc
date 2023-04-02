ac12763f3b6472474ab7ed0bd4ab68d34fa2ea3f
Rose <rose.jeantet@gmail.com>
2023-04-02 13:17:07 +0200

    switch Rating Form from Modal to inline form to match UX latest changes - tested, works

--- reactApp/src/components/ratings/AddRatingCTA.js
+++ reactApp/src/components/ratings/AddRatingCTA.js
@@ -1,32 +1,35 @@
 import { useState } from "react";
 import RatingForm from "components/ratings/RatingForm";
-import { Card, Box, Button, Typography, useTheme, useMediaQuery } from "@mui/material/";
+import { Card, Box, Button, Typography } from "@mui/material/";
 import ChevronRight from "@mui/icons-material/ChevronRight";
 
 
 function AddRatingCTA({remedy, ratings}) {
 
-const [open, setOpen] = useState(false);
-const theme = useTheme();
-const fullScreen = useMediaQuery(theme.breakpoints.down("sm"));
+const [openForm, setOpenForm] = useState(false);
+const [openCTA, setOpenCTA] = useState(true);
 
 const handleClickOpen = () => {
-    setOpen(true);
+    setOpenForm(true);
+    setOpenCTA(false);
   };
   
 
   const handleClose = () => {
-    setOpen(false);
+    setOpenForm(false);
+    setOpenCTA(true);
   };
 
 return (
     <>
+    {openCTA && 
         <Card variant="reviewCard">
             <Typography variant="body1" sx={{ textAlign: 'center' }} paragraph>
-            <>
-            {!remedy.totalNumberofRatings ?
-     "No reviews yet. Be the first to add a review" : "Did you try the remedy? Tell us what you think!"}
-  </>
+                <>
+                    {!remedy.totalNumberofRatings
+                        ? 'No reviews yet.'
+                        : 'Did you try the remedy? Tell us what you think!'}
+                </>
             </Typography>
             <Box sx={{ display: 'flex', justifyContent: 'center' }}>
                 <Button
@@ -40,13 +43,15 @@ return (
                 </Button>
             </Box>
         </Card>
-        <RatingForm
-            key={remedy.id}
-            remedy={remedy}
-            open={open}
-            handleClose={handleClose}
-            fullScreen={fullScreen}
-        />
+}
+        {openForm && (
+            <RatingForm
+                key={remedy.id}
+                remedy={remedy}
+                open={openForm}
+                handleClose={handleClose}
+            />
+        )}
     </>
 );
 }
